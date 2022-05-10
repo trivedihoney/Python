0f4f957af7e25798e2208844a4e790749944da0c
trivedihoney <102907354+trivedihoney@users.noreply.github.com>
2022-05-10 21:45:38 +0530

    Update Guessing_Game.py

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -13,4 +13,26 @@ def guess(x):
             print(f"Your guess {random_number} is correct!!")
 
 
-guess(random.randint(0, 100))
+# guess(random.randint(0, 100))
+
+
+def computer_guess():
+    print("Guess a number between 0 to 10000\n")
+    min_guess = 0
+    max_guess = 10000
+    n=0
+    guessed_number = False
+    while guessed_number != "C":
+        guessed_number_val = random.randint(min_guess, max_guess)
+        n+=1
+        guessed_number = (input(f"is {guessed_number_val} correct? Or is your guess higher or lower? C/H/L\n")).upper()
+        if guessed_number == "H":
+            min_guess = guessed_number_val+1
+        elif guessed_number == "L":
+            max_guess = guessed_number_val-1
+        elif guessed_number != "C":
+            print("Please choose from C/H/L")
+
+    print("Yay!", guessed_number_val, "is your guess!!!\n It only took", n, "Guesses! Hehe")
+
+computer_guess()
