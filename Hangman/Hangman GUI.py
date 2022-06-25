436057014de95a746773d7582c9efbeaa60ab5f2
Honey <honey.trivedi.1995@gmail.com>
2022-06-25 22:54:34 +0530

    Working on Hangman GUI

--- Hangman/Hangman GUI.py
+++ Hangman/Hangman GUI.py
@@ -9,58 +9,49 @@ from tkinter import messagebox
 from words import words
 from PIL import Image, ImageTk
 
+# get_proper_word
 def get_proper_word():
     word = random.choice(words)
     while " " in word or "-" in word:
          word = random.choice(words)
-    return word
+    return word.upper()
 
-word = get_proper_word()
-all_letters = set(string.ascii_letters)
-word_letters = set(word)
-guessed_letters = set()
-
-word_list = ["_" if letter in word_letters else letter for letter in word]
-print(" ".join(word_list))
 
 
 root = Tk()
 lives = IntVar(root,6)
-word_list_label = Label(root,text="", font=(60))
+
+word = StringVar(root,get_proper_word())
+all_letters = set(string.ascii_letters.upper())
+word_letters = StringVar(root,set(word.get()))
+guessed_letters = StringVar(root,set())
+
+word_list = ["_" if letter in word_letters.get() else letter for letter in word.get()]
+word_list_label = Label(root,text="   ".join(word_list), font=("Arial",15))
 word_list_label.grid(row =4 , column=0,columnspan=13)
 
 letters =list(set(string.ascii_letters.upper()))
 letters.sort()
 
-Livestext = Label(text=f"Lives Left : {lives.get()}", font=(24))
+Livestext = Label(text=f"Lives Left : {lives.get()}", font=(40))
 Livestext.grid(row=0,column=0,columnspan=13)
 
 #img = ImageTk.PhotoImage(Image.open("Hangman/hanged.gif"))
 #GifLabel = Label(root,image=img)
 #GifLabel.grid(row=1,column=0,columnspan=13)
 
-mainframe= Frame(height=500)
+mainframe= Frame(height=100)
 mainframe.grid(row=2,column=0,columnspan=13,rowspan=3)
+
 s = ttk.Style()
 s.configure('my.TButton', font=('Helvetica', 24))
+style = ttk.Style()
+style.configure("Gr.TButton",font=('Helvetica', 24), foreground="Green")
+style2 = ttk.Style()
+style2.configure("Red.TButton",font=('Helvetica', 24), foreground="Red")
+style3 = ttk.Style()
+style3.configure("Reset.TButton",font=('Helvetica', 24))
 
-def BtnPressed(btn_letter):
-
-    print (btn_letter)
-    
-    if btn_letter in word_list:
-        exec(f"{btn_letter}.config(bg = 'Green')")
-        word_letters.remove(btn_letter)
-        word_list_label.config(text="  ".join(word_list))
-    elif btn_letter in letters:
-        num = lives.get()
-        num-=1
-        lives.set(num)
-        Livestext.config(text=f"Lives Left : {lives.get()}")
-        if num<0:
-            messagebox(f"Game Over!! The Word was {word}")
-    else :
-        messagebox('Please enter a valid letter')
 i=0
 for x in range(5,7):
     for y in range(0, 13):
@@ -68,6 +59,48 @@ for x in range(5,7):
         exec(f"{letters[i]} = ttk.Button(text='{letters[i]}', style = 'my.TButton',width=3, command = lambda a=letters[i]: BtnPressed(a))")
         exec(f"{letters[i]}.grid(row={x},column={y})")
         if i+1 <len(letters) : i+=1
+
+def BtnPressed(btn_letter):
+
+    print (btn_letter)
+    
+    if btn_letter in word_letters.get():
+        exec(f"{btn_letter}.configure(style = 'Gr.TButton')")
+        word_letters.remove(btn_letter)
+        word_list = ["_" if letter in word_letters.get() else letter for letter in word.get()]
+        word_list_label.config(text="  ".join(word_list))
+        if len(word_letters.get())<1:
+            messagebox.showinfo("WIN",'Yay!!! You guessed the word!!')
+            guessed_letters.clear()
+            resetAll()
+
+    elif btn_letter in guessed_letters.get():
+        pass
+    elif btn_letter in letters:
+        num = lives.get()
+        num-=1
+        lives.set(num)
+        Livestext.config(text=f"Lives Left : {lives.get()}")
+        guessed_letters.add(btn_letter)
+        exec(f"{btn_letter}.configure(style = 'Red.TButton')")
+        if num<1:
+            messagebox.showinfo("GAME OVER",f"Game Over!! The Word was {word.get()}")
+            guessed_letters.clear()
+            resetAll()
+    else :
+        messagebox.showinfo("ERROR",'Please enter a valid letter')
+
+def resetAll():
+    
+    lives.set(6)
+    Livestext.config(text=f"Lives Left : {lives.get()}")
+    word.set (get_proper_word())
+    word_letters = set(word.get())
+    word_list = ["_" if letter in word_letters.get() else letter for letter in word.get()]
+    word_list_label.config(text="  ".join(word_list))
+
+    for btn_letter in letters:
+        exec(f"{btn_letter}.configure(style = 'Reset.TButton',width=3)")
 
 
 root.mainloop()
