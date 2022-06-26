4cf8576207baeae8e7cf6762e2d62787612764c2
Honey <honey.trivedi.1995@gmail.com>
2022-06-26 15:46:54 +0530

    Made simple rock paper scissors game

---
+++ Rock paper Scissors/RockPaperScissors.py
@@ -0,0 +1,23 @@
+import random
+
+def playgame():
+    playerChoice = input('Choose any one option\n"r" for Rock, "p" for paper, "s" for sciossors\n')
+    while playerChoice!= "r" and "p" and "s":
+        print("Invalid choice")
+        playerChoice = input('Choose any one option:\n"r" for Rock, "p" for paper, "s" for sciossors\n')
+    
+    computerChoice = random.choice(["r","p","s"])
+    #print (computerChoice)
+    #r>s,s>p,p>r
+    result =isWin(playerChoice,computerChoice)
+    print(result)
+
+def isWin(p,c):
+    if p==c:
+        return "It's a Draw"
+    if (p=="r" and c=="s") or (p=="s" and c=="p") or (p=="p" and c=="r"):
+        return "You Win!!"
+    else:
+        return "You Lost!!"
+
+playgame()
