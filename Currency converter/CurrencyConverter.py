30d9930128162e99a1580f3e75cb604fc1c444b5
Honey <honey.trivedi.1995@gmail.com>
2022-06-27 22:49:08 +0530

    Converter file made, no GUI

--- Currency converter/CurrencyConverter.py
+++ Currency converter/CurrencyConverter.py
@@ -2,4 +2,20 @@
 import requests
 
 class RealTimeCurrencyConverter():
-    pass
+    def __init__(self,url) :
+        self.data = requests.get(url).json()
+        self.currencies = self.data["rates"]
+
+    def convert(self,fromCurrency,toCurrency,amount):
+        initial_amount = amount
+        if fromCurrency!="USD":
+            amount=amount/self.currencies[fromCurrency]
+            amount = round(amount*self.currencies[toCurrency],4)
+            return amount
+
+
+url ="https://open.er-api.com/v6/latest/USD"
+
+conveter = RealTimeCurrencyConverter(url)
+print(conveter.convert("INR","USD",100))
+print(conveter.data)
