55135e70e61ed3d6a4d46c67258bd202e4bf4fd8
Honey <honey.trivedi.1995@gmail.com>
2022-07-24 18:31:50 +0530

    Modified web scraper to handle server errors

--- Web Scraper/AmazonWebScraper.py
+++ Web Scraper/AmazonWebScraper.py
@@ -1,4 +1,3 @@
-from symtable import Symbol
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,23 +5,30 @@ SearchString=input("Input an item to search in amazon\n")
 SearchString = SearchString.replace(" " , "+")
 url = "https://www.amazon.in/s?k=" + SearchString
 re=requests.get(url)
-#print(re)
-re_html = re.text
-soup = BeautifulSoup(re_html, features="lxml")
+
+if re.status_code == 200:
+
+    re_html = re.text
+    soup = BeautifulSoup(re_html, features="lxml")
 
 
 
+    print(soup.prettify())
+    search_list = soup.find("span",class_="rush-component s-latency-cf-section")
+    print(search_list)
 
-search_list = soup.find("span",class_="rush-component s-latency-cf-section")
-results = search_list.find_all("div",class_="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20" )
 
-for result in results:
-    name = result.find("span",class_="a-size-base-plus a-color-base a-text-normal").text
-    if result.find("span",class_="a-price-symbol") is not None:
-        sym = result.find("span",class_="a-price-symbol").text
-        price = result.find("span",class_="a-price-whole").text
-    else:
-        sym=""
-        price=""
+    results = search_list.find_all("div",class_="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20" )
 
-    print(name + "\n" +sym + price +"\n")
+    for result in results:
+        name = result.find("span",class_="a-size-base-plus a-color-base a-text-normal").text
+        if result.find("span",class_="a-price-symbol") is not None:
+            sym = result.find("span",class_="a-price-symbol").text
+            price = result.find("span",class_="a-price-whole").text
+        else:
+            sym=""
+            price=""
+
+        print(name + "\n" +sym + price +"\n")
+else:
+    print("Error with server communication")
